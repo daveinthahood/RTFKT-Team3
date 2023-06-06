4a4008a6eaddb43384c24287ae9a362ffa9706b1
SSbrilly <Simonesbrilli@gmail.com>
2023-06-06 21:57:00 +0200

    feat: js done

--- Component/navbar/dropdown.js
+++ Component/navbar/dropdown.js
@@ -1,1 +1,19 @@
+const collectionLink = document.querySelector("#collections-link");
 
+const floatingMenu = document.querySelector("#floating-menu");
+
+function showMenu() {
+  floatingMenu.classList.add("visible");
+}
+
+function hideMenu() {
+  floatingMenu.classList.remove("visible");
+}
+
+collectionLink.addEventListener("mouseover", showMenu);
+
+floatingMenu.addEventListener("mouseover", showMenu);
+
+collectionLink.addEventListener("mouseout", hideMenu);
+
+floatingMenu.addEventListener("mouseout", hideMenu);
