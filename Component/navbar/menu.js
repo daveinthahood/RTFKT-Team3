4b450901c1206edd11d47136aa7325cbdd75f87a
SSbrilly <Simonesbrilli@gmail.com>
2023-06-14 02:57:35 +0200

    commit for pc change

--- Component/navbar/menu.js
+++ Component/navbar/menu.js
@@ -1,28 +1,49 @@
-// const $body = document.querySelector("#body-for-menu-onclick");
-const $menu = document.querySelector("#menu-button__modal");
+// // const $body = document.querySelector("#body-for-menu-onclick");
+// const $menu = document.querySelector("#menu-button__modal");
+// const $signInButton = document.querySelector("#signIn-navbar");
+// const $buttonBX = document.querySelector("#buttonBX");
+// const $burgerSVG = document.querySelector("#burger");
+// const $xSVG = document.querySelector("#burger-close");
+
+// $buttonBX.addEventListener("click", () => {
+//   if ($menu.classList.contains("active")) {
+//     $menu.classList.remove("active");
+//     $menu.style.display = "none";
+//     $burgerSVG.style.display = "block";
+//     $signInButton.style.display = "block";
+//     $xSVG.style.display = "none";
+//   } else {
+//     $menu.classList.add("active");
+//     $menu.style.display = "block";
+//     $burgerSVG.style.display = "none";
+//     $signInButton.style.display = "none";
+//     $xSVG.style.display = "block";
+//   }
+// });
+
+const $body = document.querySelector("#body-for-menu-onclick");
+const $modal = document.querySelector("#menu-button__modal");
 const $signInButton = document.querySelector("#signIn-navbar");
+const $buttonBX = document.querySelector("#buttonBX");
 const $burgerSVG = document.querySelector("#burger");
-const $xSVG = (document = document.querySelector("#burger-close"));
+const $xSVG = document.querySelector("#burger-close");
 
-function showMenu() {
-  let menuBox = document.getElementById("menu-button__modal");
-  let signInButton = document.getElementById("signIn-navbar");
-  let burgerSVG = document.getElementById("burger");
-  let xSVG = document.getElementById("burger-close");
-  //   let body = document.getElementById("body-for-menu-onclick");
-  if (menuBox.style.display == "none") {
-    menuBox.style.display = "flex";
-    signInButton.style.display = "none";
-    burgerSVG.style.display = "none";
-    xSVG.style.display = "flex";
-    // body.style.display = "none";
+$buttonBX.addEventListener("click", () => {
+  if ($modal.classList.contains("active")) {
+    $modal.classList.remove("active");
+    $body.style.overflow = "initial";
+    $modal.style.display = "none";
+    $burgerSVG.style.display = "block";
+    $signInButton.style.display = "block";
+    $xSVG.style.display = "none";
   } else {
-    menuBox.style.display = "none";
-    signInButton.style.display = "flex";
-    burgerSVG.style.display = "flex";
-    xSVG.style.display = "none";
-    // body.style.display = "flex";
+    $modal.classList.add("active");
+    $body.style.overflow = "hidden"
+    // $menu.style.display = "flex";
+    $modal.style.display = "block";
+    $burgerSVG.style.display = "none";
+    $signInButton.style.display = "none";
+    $xSVG.style.display = "block";
   }
-}
+});
 
-showMenu();
