dd9f26dabdb825fa78b5bc8e791091471d0f1dd5
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-06-21 18:14:34 +0200

    add comment on js

--- Component/discord-banner/discordBanner.js
+++ Component/discord-banner/discordBanner.js
@@ -1,3 +1,4 @@
+const $discordBannerFirstWinner = document.querySelector(".discord-banner-winner-first");
 const $discordBannerSecondWinner = document.querySelector(".discord-banner-winners-second"); //  elemento su cui inserire l'hover
 const $discordBannerThirdWinner = document.querySelector(".discord-banner-winners-third"); // elemento su cui inserire l'hover
 const $discordBannerCenterImg = document.querySelector(".discord-banner-center-image"); // dove cambiare immagine
@@ -6,29 +7,31 @@ const $creator2 = document.querySelector(".creator2"); // secondo creator pic
 const $creator3 = document.querySelector(".creator3"); // terzo creator pic 
 
 
-//creo l'event listner sul banner 
+//creo l'event listner sul banner numero 2 
 $discordBannerSecondWinner.addEventListener('mouseover', () => {
 
-    $discordBannerCenterImg.innerHTML = $creator2.style.display;
+    $discordBannerCenterImg.innerHTML = $creator2;
     console.log("funciont");
 })
 
 $discordBannerSecondWinner.addEventListener('mouseleave', () => {
     
-    $discordBannerCenterImg.innerHTML = $creator1.style.display;
-    
+    $discordBannerCenterImg.innerHTML = $creator1;
+
     console.log("CIAO 2");
 })
 
+
+//creo l'event listner sul banner numero 3
 $discordBannerThirdWinner.addEventListener('mouseover', () => {
     
-    $discordBannerCenterImg.innerHTML = $creator3.style.display;
+    $discordBannerCenterImg.innerHTML = $creator3;
     console.log("OK");
 })
 
 $discordBannerThirdWinner.addEventListener('mouseleave', () => {
    
-    $discordBannerCenterImg.innerHTML = $creator1.innerHTML;
+    $discordBannerCenterImg.innerHTML = $creator1;
     console.log("CIAO3");
 })
 
