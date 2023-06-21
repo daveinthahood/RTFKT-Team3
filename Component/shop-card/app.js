705604d4b6dc2471340820b81389550eb5cf841a
grinker <gabriele.barberio@gmail.com>
2023-06-21 17:34:09 +0200

    feat: removed plus code

--- Component/shop-card/app.js
+++ Component/shop-card/app.js
@@ -1,6 +1,5 @@
 const $card = document.querySelector(".section__shop__card");
 const $vid = document.querySelector("video");
-const $vidBox = document.querySelector(".section__shop__card-vid")
 
 $card.addEventListener("mouseenter", () => {
     $vid.play();
