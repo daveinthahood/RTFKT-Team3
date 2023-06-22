f4b6c3f26eec3234dc8f35988ebb4cb17399dda7
grinker <gabriele.barberio@gmail.com>
2023-06-22 13:11:29 +0200

    feat: init hide signle categorie feat

--- Component/filter-menu/app.js
+++ Component/filter-menu/app.js
@@ -6,5 +6,13 @@ document.addEventListener("click",(event) =>{
     if (target.classList.contains("button--hide")){
         $menu.style.display = $menu.style.display === "flex" ? "none" : "flex";
     }
+
+    if(target.classList.contains("clear")){
+        console.log("clear")
+    } 
+
+    if(target.classList.contains("show")){
+        console.log("show")
+    }
 }
 )
