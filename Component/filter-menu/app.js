cb66c2dfd349f512d1d8decc3989478b482133b2
grinker <gabriele.barberio@gmail.com>
2023-06-23 17:03:30 +0200

    feat: fixed hide btn event listener

--- Component/filter-menu/app.js
+++ Component/filter-menu/app.js
@@ -1,14 +1,14 @@
 const $menu = document.querySelector(".aside");
-const $creators = document.querySelector(".creators")
-const $categories = document.querySelector(".categories")
+const $creators = document.querySelector(".creators");
+const $categories = document.querySelector(".categories");
+const $btnHide = document.querySelector(".button--hide");
+
+$btnHide.addEventListener('click', () => {
+    $menu.style.display = $menu.style.display === "flex" ? "none" : "flex";
+})
 
 document.addEventListener("click",(event) =>{
     const target = event.target;
-
-    if (target.classList.contains("button--hide")){
-        $menu.style.display = $menu.style.display === "flex" ? "none" : "flex";
-    }
-
     if(target.classList.contains("clearcategories")){
         console.log("clear")
         $categories.style.display = "none";
