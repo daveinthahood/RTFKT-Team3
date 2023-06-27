10d69a13697538eabc06b244e1fb0f7b5fcf2ad8
grinker <gabriele.barberio@gmail.com>
2023-06-27 13:25:45 +0200

    feat: open up input search when nutton is clicked

--- Component/dropsarchive/app.js
+++ Component/dropsarchive/app.js
@@ -3,18 +3,27 @@ const $creators = document.querySelector(".creators");
 const $categories = document.querySelector(".categories");
 const $hideButton = document.querySelector("#hide");
 const $buttonShow = document.querySelector("#show");
-const $buttonSearch = document.querySelector("#searchbtn")
+const $buttonSearch = document.querySelector("#searchbtn");
+const $inputSearch = document.querySelector("#searchbox");
 
 
 $hideButton.addEventListener("click", ()=> {
     $menu.style.display = $menu.style.display === "flex" ? "none" : "flex";
     $buttonShow.style.display = $buttonShow.style.display === "none" ? "block" : "none";
-    $buttonSearch.style.right = "-35px";
+    $buttonSearch.style.right = "-30px";
+    $inputSearch.style.right = "-33px";
+
 })
 
 $buttonShow.addEventListener("click", () => {
     $menu.style.display = $menu.style.display === "none" ? "flex" : "none";
     $buttonShow.style.display = $buttonShow.style.display === "block" ? "none" : "none";
+})
+
+$buttonSearch.addEventListener("click", () => {
+    $inputSearch.style.transform = "scale(1)";
+    $inputSearch.style.opacity = "1";
+
 })
 
 // document.addEventListener("click", (event) => {
