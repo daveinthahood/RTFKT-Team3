dfe1e7d20c330f0c3c16279170defbd3b0561448
grinker <gabriele.barberio@gmail.com>
2023-06-27 16:31:21 +0200

    feat: added multiply layout

--- Component/dropsarchive/app.js
+++ Component/dropsarchive/app.js
@@ -9,7 +9,30 @@ const $buttonCloseSrc = document.querySelector("#clssrcbtn");
 const $clearCat = document.querySelector("#clearcat");
 const $showCat = document.querySelector("#showcat");
 const $showCreat = document.querySelector("#showcreat");
-const $clearCreat = document.querySelector("#clearcreat")
+const $clearCreat = document.querySelector("#clearcreat");
+const $large = document.querySelector("#large");
+const $small = document.querySelector("#small");
+const $card = document.querySelectorAll(".section__shop__main__card");
+
+$large.addEventListener("click", () => {
+    $small.classList.add("transparent");
+    $large.classList.remove("transparent");
+    $large.style.transform = "scale(1.3)";
+    $small.style.transform = "";
+    [...$card].forEach(el => {
+        el.style.flex = "1 0 45%"
+    })
+})
+
+$small.addEventListener("click", () => {
+    $large.classList.add("transparent");
+    $small.classList.remove("transparent");
+    $small.style.transform = "scale(1.3)";
+    $large.style.transform = "";
+    [...$card].forEach(el => {
+        el.style.flex = "1 0 30%"
+    })
+})
 
 //*nasconde il menù filtri
 $hideButton.addEventListener("click", ()=> {
@@ -23,12 +46,13 @@ $hideButton.addEventListener("click", ()=> {
 //*mostra il emnu filtri
 $buttonShow.addEventListener("click", () => {
     $menu.style.display = $menu.style.display === "none" ? "flex" : "none";
-    $buttonShow.style.display = $buttonShow.style.display === "block" ? "none" : "none";
+    $buttonShow.style.display = $buttonShow.style.display === "block" ? "none" : "block";
 })
 //* animazione input ricerca apertura
 $buttonSearch.addEventListener("click", () => {
     $inputSearch.style.transform = "translateX(1)";
     $inputSearch.style.opacity = "1";
+    // $buttonSearch.style.display = "none";
     $buttonCloseSrc.style.display = "block";
 })
 
@@ -36,7 +60,7 @@ $buttonSearch.addEventListener("click", () => {
 $buttonCloseSrc.addEventListener("click", () => {
     $inputSearch.style.transform = "";
     $inputSearch.style.opacity = "";
-    $buttonCloseSrc.style.display = "";
+    $buttonCloseSrc.style.display = "none";
 })
 
 
@@ -54,12 +78,3 @@ $clearCreat.addEventListener("click", () => {
 $showCreat.addEventListener("click", () => {
     $creators.style.display = "flex";
 })
-//  //?hide e show della categoria creators
-//   if (target.classList.contains("clearcreators")) {
-//     $creators.style.display = "none";
-//   }
-
-//   if (target.classList.contains("showcreators")) {
-//     $creators.style.display = "flex";
-//   }
-// });
