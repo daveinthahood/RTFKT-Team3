c53c0eed9403cbfe561c79dcd0784b89852dbd1a
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-06-29 18:41:20 +0200

    feat js whoweare

--- Component/who-we-are/whoweare.js
+++ Component/who-we-are/whoweare.js
@@ -15,10 +15,10 @@ const $myPic4 = document.getElementById('picHover4');
 const $myPic5 = document.getElementById('picHover5'); 
 const $myPic6 = document.getElementById('picHover6');
 const $myPic7 = document.getElementById('picHover7'); 
-const $myPic8 = document.getElementById('picHover'); 
+const $myPic8 = document.getElementById('picHover8'); 
 
 // prima immagine 
-$span.addEventListener("mousemove", (event) => {
+$span.addEventListener("mouseover", (event) => {
     const target = event.target;
     if(target.classList.contains("pic")){
         $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/creator.gif";   
@@ -33,7 +33,7 @@ $span.addEventListener("mouseleave", (event) => {
 $span.addEventListener('mousemove', (event) => {
     $myPic.style.display = "inherit";
     $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/creator.gif";   
-    $myPic.style.top =`${event.clientX/100}px` ;
+    $myPic.style.top =`${event.clientX/-10}px` ;
     $myPic.style.left = `${event.clientY/100}px`;
     console.log(event.clientY,event.clientX);
     $myPic.style.zIndex = "1";
@@ -48,6 +48,7 @@ $span1.addEventListener("mouseover", (event) => {
         $myPic1.src="https://cdn.rtfkt.com/assets/whoarewe/RTFKT.png";   
     }
 })
+
 $span1.addEventListener("mouseleave", (event) => {
     $myPic1.style.display= "none";  
 })
@@ -55,8 +56,8 @@ $span1.addEventListener("mouseleave", (event) => {
 $span1.addEventListener('mousemove', (event) => {
     $myPic1.style.display = "inherit";
     $myPic1.src="https://cdn.rtfkt.com/assets/whoarewe/RTFKT.png";   
-    $myPic1.style.top =`${event.clientX/100}px` ;
-    $myPic1.style.left = `${event.clientY/100}px`;
+    $myPic1.style.top =`${event.clientX/-20}px` ;
+    $myPic1.style.left = `${event.clientY/ -10}px`;
     console.log(event.clientY,event.clientX);
     $myPic1.style.zIndex = "1";
     $myPic1.style.opacity = "1";
@@ -67,24 +68,23 @@ $span1.addEventListener('mousemove', (event) => {
 $span2.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic2")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/game-engine.png";   
+        $myPic2.src="https://cdn.rtfkt.com/assets/whoarewe/game-engine.png";   
     }
 })
 $span2.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic2")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic2.style.display= "none";  
 })
+
 $span2.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic2.style.display = "inherit";
+    $myPic2.src="https://cdn.rtfkt.com/assets/whoarewe/game-engine.png";   
+    $myPic2.style.top =`${event.clientX/-20}px` ;
+    $myPic2.style.left = `${event.clientY/ -10}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic2.style.zIndex = "1";
+    $myPic2.style.opacity = "1";
   })
+
 
 
 
@@ -93,24 +93,23 @@ $span2.addEventListener('mousemove', (event) => {
 $span3.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic3")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/clonex-nft.png";   
+        $myPic3.src="https://cdn.rtfkt.com/assets/whoarewe/clonex-nft.png";   
     }
 })
 $span3.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic3")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic3.style.display= "none";  
 })
+
 $span3.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic3.style.display = "inherit";
+    $myPic3.src="https://cdn.rtfkt.com/assets/whoarewe/clonex-nft.png";   
+    $myPic3.style.top =`${event.clientX/-10}px` ;
+    $myPic3.style.left = `${event.clientY/ -10}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic3.style.zIndex = "1";
+    $myPic3.style.opacity = "1";
   })
+
 
 
 
@@ -119,47 +118,42 @@ $span3.addEventListener('mousemove', (event) => {
 $span4.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic4")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/blockchain.png";   
+        $myPic4.src="https://cdn.rtfkt.com/assets/whoarewe/blockchain.png";   
     }
 })
 $span4.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic4")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic4.style.display= "none";  
 })
 $span4.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic4.style.display = "inherit";
+    $myPic4.src="https://cdn.rtfkt.com/assets/whoarewe/blockchain.png";   
+    $myPic4.style.top =`${event.clientX/-10}px` ;
+    $myPic4.style.left = `${event.clientY/0}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic4.style.zIndex = "1";
+    $myPic4.style.opacity = "1";
   })
+
 
 
 // Sesta Immagine
 $span5.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic5")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/augmented-reality.png";   
+        $myPic5.src="https://cdn.rtfkt.com/assets/whoarewe/augmented-reality.png";   
     }
 })
 $span5.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic5")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic5.style.display= "none";  
 })
 $span5.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic5.style.display = "inherit";
+    $myPic5.src="https://cdn.rtfkt.com/assets/whoarewe/augmented-reality.png";   
+    $myPic5.style.top =`${event.clientX/-30}px` ;
+    $myPic5.style.left = `${event.clientY/100}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic5.style.zIndex = "1";
+    $myPic5.style.opacity = "1";
   })
 
 
@@ -168,23 +162,20 @@ $span5.addEventListener('mousemove', (event) => {
 $span6.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic6")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/manufacturing.png";   
+        $myPic6.src="https://cdn.rtfkt.com/assets/whoarewe/manufacturing.png";   
     }
 })
 $span6.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic6")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic6.style.display= "none";  
 })
 $span6.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic6.style.display = "inherit";
+    $myPic6.src="https://cdn.rtfkt.com/assets/whoarewe/manufacturing.png";   
+    $myPic6.style.top =`${event.clientX/-10}px` ;
+    $myPic6.style.left = `${event.clientY/100}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic6.style.zIndex = "1";
+    $myPic6.style.opacity = "1";
   })
 
 
@@ -193,24 +184,22 @@ $span6.addEventListener('mousemove', (event) => {
 $span7.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic7")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/sneakers.png";   
+        $myPic7.src="https://cdn.rtfkt.com/assets/whoarewe/sneakers.png";   
     }
 })
 $span7.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic7")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic7.style.display= "none";  
 })
 $span7.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic7.style.display = "inherit";
+    $myPic7.src="https://cdn.rtfkt.com/assets/whoarewe/sneakers.png";   
+    $myPic7.style.top =`${event.clientX /0}px` ;
+    $myPic7.style.left = `${event.clientY/ 50}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic7.style.zIndex = "1";
+    $myPic7.style.opacity = "1";
   })
+
 
 
 
@@ -219,22 +208,20 @@ $span7.addEventListener('mousemove', (event) => {
 $span8.addEventListener("mousemove", (event) => {
     const target = event.target;
     if(target.classList.contains("pic8")){
-        const id = target.dataset.id;
-        $myPic.src="https://cdn.rtfkt.com/assets/whoarewe/digital-artefacts.png";   
+        $myPic8.src="https://cdn.rtfkt.com/assets/whoarewe/digital-artefacts.png";   
     }
 })
 $span8.addEventListener("mouseleave", (event) => {
-    const target = event.target;
-    if(target.classList.contains("pic8")){
-        const id = target.dataset.id;
-        $myPic.src="";   
-    }
+    $myPic8.style.display= "none";  
 })
 $span8.addEventListener('mousemove', (event) => {
-    const cursor = document.querySelector('#picHover');
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.zIndex = "1";
-    cursor.style.opacity = "1";
+    $myPic8.style.display = "inherit";
+    $myPic8.src="https://cdn.rtfkt.com/assets/whoarewe/digital-artefacts.png";   
+    $myPic8.style.top =`${event.clientX /0}px` ;
+    $myPic8.style.left = `${event.clientY/0}px`;
+    console.log(event.clientY,event.clientX);
+    $myPic8.style.zIndex = "1";
+    $myPic8.style.opacity = "1";
   })
+
 
